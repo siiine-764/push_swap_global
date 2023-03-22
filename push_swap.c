96b860ec5f5ec85f0530d3966ee730716d780b33
mayache- <mayache-@student.1337.ma>
2023-03-22 22:29:54 +0000

    task to check input (double number && alphabetek)

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mayache- <mayache-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 14:49:13 by mayache-          #+#    #+#             */
-/*   Updated: 2023/03/20 15:00:51 by mayache-         ###   ########.fr       */
+/*   Updated: 2023/03/22 22:28:13 by mayache-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,12 +99,12 @@ void	ft_check_range(t_stack *s)
 	int		range = 0;
 	long long	*chaos = ft_sort(s);
 	long long	sopa = 0; 
-	int y = 0;
-	while (chaos[y])
-	{
-		printf("chaos[%d] = %lld\n", y, chaos[y]);
-		y++;
-	}
+	// int y = 0;
+	// while (chaos[y])
+	// {
+	// 	printf("chaos[%d] = %lld\n", y, chaos[y]);
+	// 	y++;
+	// }
 	while (s->top_a >= 0)
 	{
 		sopa = ft_find_local(chaos, s->stack_a[0]);
@@ -124,6 +124,61 @@ void	ft_check_range(t_stack *s)
 	}
 }
 
+void	check_argv(int argc, char **argv)
+{
+	int		i;
+	char	*str;
+
+	i = 1;
+	while (i != argc)
+	{
+		str = argv[i];
+		while (*str)
+		{
+			if (*str == '-' || *str == '+' )
+			{
+				str++;
+				if (*str < '0' || *str > '9')
+				{
+					write(1, "error VIP\n", 10);
+				exit(1);
+					
+				}
+			}
+			else if (*str != ' ' && (*str < '0' || *str > '9'))
+			{
+				write(1, "error VIP\n", 10);
+				exit(1);
+			}
+			if (*str)
+				str++;
+		}
+		i++;
+	}
+}
+
+void	ft_check_nbr(char **av)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (av[i])
+	{
+		j =  i + 1;
+		while(av[j])
+		{
+			if (ft_atoi(av[i]) == ft_atoi(av[j]))
+			{
+				write(1, "Error VIP : Double number\n", 26);
+				exit(1);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 int main(int ac, char **av)
 {
 	char	**nmbr;
@@ -133,9 +188,10 @@ int main(int ac, char **av)
 	if (ac == 1)
 		ft_message_error_empty();
 
-	// check_argv(ac, av);
+	check_argv(ac, av);
 	// ft_check_same_nbr(av);
 	nmbr = put_nbr_array(av, ac, &s);
+	ft_check_nbr(nmbr);
 	s.stack_a = malloc(sizeof(long long) * (s.size + 1));
 	s.stack_b = malloc(sizeof(long long) * (s.size + 1));
 	fill_stack_a(nmbr, &s);
@@ -147,7 +203,7 @@ int main(int ac, char **av)
 	// print_stacks(&s);
 	ft_push_stack_a(&s);
 	// while(1);
-	print_stacks(&s);
+	// print_stacks(&s);
 	// int i = ft_sorted(s.stack_a, s.top_a);
 	// if (i == 0)
 	// {
